ab6ab869b6d02bd58410f6c08dc319b79a02925a
“dreamfly11712” <1171271548@qq.com>
2023-03-14 23:14:43 +0800

    add

---
+++ juc/src/main/java/org/example/Main.java
@@ -0,0 +1,11 @@
+package org.example;
+
+/**
+ * @Author young
+ * @Date ${YEAR}-${MONTH}-${DAY} ${HOUR}:${MINUTE}
+ **/
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
